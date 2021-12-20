5ac8f7b629abb235036d4c1ce6bd61fa9bb85afa
Alexandre Morey <alexander006_9@hotmail.com>
2021-12-20 17:13:22 +0100

    last commit before fullcalendar gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one :company
+  belongs_to :company
 
   has_many :host_schedules, class_name: 'Schedule', foreign_key: 'host_id'
   has_many :hostee_schedules, class_name: 'Schedule', foreign_key: 'hostee_id'
