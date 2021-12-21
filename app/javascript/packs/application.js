97c0962066f6a7f22c2190501dd92a6cd1194e04
Alexandre Morey <alexander006_9@hotmail.com>
2021-12-21 16:55:50 +0100

    removed the full calendar gem

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,17 +3,19 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-//= require moment 
-//= require fullcalendar
+
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
 
 
 // ----------------------------------------------------
