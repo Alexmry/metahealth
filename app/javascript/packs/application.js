5a820a8380fd058682bb083405fe2eab8e5dbbf0
Alexandre Morey <alexander006_9@hotmail.com>
2021-12-22 14:41:01 +0100

    work done with Ali

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,12 +25,27 @@ ActiveStorage.start()
 
 // External imports
 import "bootstrap";
-
+import "moment"; 
+import "fullcalendar";
+require("jquery");
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+
+  eventCalendar();  
+
 });
 
+function eventCalendar() {
+  return $('#calendar').fullCalendar({ });
+};
+function clearCalendar() {
+  $('#calendar').fullCalendar('delete'); 
+  $('#calendar').html('');
+};
+
+
+$(document).on('turbolinks:before-cache', clearCalendar);
